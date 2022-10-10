274e02b94b530772c54bb9a82b9d54b7dda8fb8a
Jithin Ramesh <109571606+jithinramesh@users.noreply.github.com>
2022-10-10 10:51:33 +0530

    hgc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,43 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdddonorComponent } from './adddonor/adddonor.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SearchdonorComponent } from './searchdonor/searchdonor.component';
+import { DeletedonorComponent } from './deletedonor/deletedonor.component';
+import { RouterModule,Routes  } from '@angular/router';
+import { ViewallComponent } from './viewall/viewall.component';
+
+
+const appRoutes : Routes=[
+  {
+    path:"",component:AdddonorComponent
+  },
+  {
+    path:"search",component:SearchdonorComponent
+  },
+  {
+    path:"delete",component:DeletedonorComponent
+  },
+  {
+    path:"viewall",component:ViewallComponent
+  }
+
+]
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     AdddonorComponent,
     NavbarComponent,
-  
+    SearchdonorComponent,
+    DeletedonorComponent,
+    ViewallComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
